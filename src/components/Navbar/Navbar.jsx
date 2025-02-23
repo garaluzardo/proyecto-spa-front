4c0fc3bbcd5a5f5b00fd0d517766441131f961f2
Gara Luzardo <garaluzardo@gmail.com>
2025-02-23 10:27:29 +0000

    Corrección logo navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { useNavigate, Link } from "react-router-dom";
 import "./Navbar.css";
 import ProfPic from "../../images/profile-pic-cropped.jpg";
 import LogoBN from "../../images/Logos/logo-bn.png";
-import LogoColor from "../../images/Logos/logo-bn.png";
+import LogoColor from "../../images/Logos/logo-cl.png";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -12,8 +12,8 @@ const Navbar = () => {
       {/* Sección izquierda */}
       <div className="left-section">
       <Link to="/BooksList" className="logo-container">
-          <img src={"src/images/Logos/logo-bn.png"} alt="Logo" className="logo-bn" />
-          <img src={"src/images/Logos/logo-cl.png"} alt="Logo" className="logo-color" />
+          <img src={LogoBN} alt="Logo" className="logo-bn" />
+          <img src={LogoColor} alt="Logo" className="logo-color" />
           </Link>
         <p className="title"><b>Book</b>Nook</p>
       </div>
